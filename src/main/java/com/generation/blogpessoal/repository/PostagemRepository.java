0b1b102ec67ba891fc23fd6cc8ac4b14dd34d6fb
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-12 12:40:38 -0300

    Created custom JPA query methods

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -2,8 +2,16 @@ package com.generation.blogpessoal.repository;
 
 import com.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    //Métodos personalizados utilizando o JPA
+    //SELECT * FROM tb_postagens WHERE titulo LIKE "%titulo%"
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
 }
