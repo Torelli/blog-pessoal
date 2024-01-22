820d0207d57e3358deba5aafd880edfc0c1801a4
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-22 13:43:07 -0300

    Updated PostagemController Put and Post methods

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,6 +2,7 @@ package com.generation.blogpessoal.controller;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,9 @@ public class PostagemController {
 
     @Autowired
     private PostagemRepository postagemRepository;
+
+    @Autowired
+    private TemaRepository temaRepository;
 
     @GetMapping
     public ResponseEntity<List<Postagem>> findAll() {
@@ -39,8 +43,10 @@ public class PostagemController {
 
     @PostMapping
     public ResponseEntity<Postagem> createPostagem(@Valid @RequestBody Postagem postagem) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(postagemRepository.save(postagem));
+        if (temaRepository.existsById(postagem.getTema().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(postagemRepository.save(postagem));
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema Inválido", null);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -48,7 +54,7 @@ public class PostagemController {
     public void deleteById(@PathVariable Long id) {
         Optional<Postagem> postagem = postagemRepository.findById(id);
 
-        if(postagem.isEmpty())
+        if (postagem.isEmpty())
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
         postagemRepository.deleteById(id);
@@ -56,9 +62,14 @@ public class PostagemController {
 
     @PutMapping
     public ResponseEntity<Postagem> updatePostagem(@Valid @RequestBody Postagem postagem) {
-        return postagemRepository.findById(postagem.getId())
-                .map(response -> ResponseEntity.status(HttpStatus.OK)
-                        .body(postagemRepository.save(postagem)))
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        if(postagemRepository.existsById(postagem.getId())) {
+            if (temaRepository.existsById(postagem.getTema().getId()))
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(postagemRepository.save(postagem));
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema Inválido", null);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
