e81aedef0458d6d58806a7b0e4748913dfe4eb32
Giovanni Torelli <giovanni.walter@outlook.com>
2024-02-28 12:18:30 -0300

    allowed all headers to update method

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -50,7 +50,6 @@ public class UsuarioController {
                 .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
 
-    @CrossOrigin(origins = "https://postlab.vercel.app", allowedHeaders = "*")
     @PutMapping("/atualizar")
     public ResponseEntity<Usuario> updateUsuario(@Valid @RequestBody Usuario usuario) {
         return usuarioService.atualizarUsuario(usuario)
